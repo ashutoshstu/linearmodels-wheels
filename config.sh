7148454269db331f652accba2f59b8519861d2f8
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2021-10-11 17:49:46 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,13 @@
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    wget https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh -O miniconda.sh
+    MINICONDA_PATH=/home/travis/miniconda
+    chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
+    export PATH=$MINICONDA_PATH/bin:$PATH
+    conda install -c conda-forge statsmodels
+}
+
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
