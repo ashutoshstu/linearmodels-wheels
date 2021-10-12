243dc08ee44c4e2b9bcac32d882909aa473cf95c
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2021-10-12 10:49:50 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
+    python --version
     if [ "$uname -m" == aarch64 ]; then
     yum -y install wget
     wget https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh -O miniconda.sh
